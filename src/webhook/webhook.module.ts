385b9d2fcc8261cbf4759de8cd85ea44b38615c8
Rifat79 <ibrahim.rifat.127700@gmail.com>
2025-10-29 00:37:31 +0600

    fix(webhook module): added WebhookResultScheduler in webhook module

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HttpClientModule } from 'src/common/http-client/http-client.module';
 import { ProductModule } from 'src/product/product.module';
+import { WebhookResultScheduler } from 'src/schedulers/webhook-result.scheduler';
 import { WebhookService } from './webhook.service';
 
 @Module({
   imports: [HttpClientModule, ProductModule],
-  providers: [WebhookService],
+  providers: [WebhookService, WebhookResultScheduler],
   exports: [WebhookService],
 })
 export class WebhookModule {}
